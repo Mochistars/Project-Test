c09a0c3ffca1b0554dc288be4ac28115fa843798
Mochistars <mochistars9@gmail.com>
2025-07-03 13:49:32 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const express =require('express');
+const app = express();
+const port=3000;
+app.use(express.static(__dirname))
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname + '/home.html');
+});
+
+app.get('/about',(req,res)=>{
+    res.sendFile(__dirname + '/about.html');
+});
+app.get('/home',(req,res)=>{
+    res.sendFile(__dirname + '/home.html');
+});
+app.get('/externalLinks',(req,res)=>{
+    res.sendFile(__dirname + '/externalLinks.html');
+});
+
+app.listen(port,()=>{
+    console.log(`Server is running at http://localhost:${port}`)
+});
